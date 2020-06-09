4e79410f9de4f73b9c46153ca953d4ba4a70a46a
MYF12 <1016401546@qq.com>
2020-06-09 16:34:00 +0800

    test
    Signed-off-by: MYF12 <1016401546@qq.com>

---
+++ 输入.py
@@ -0,0 +1,18 @@
+#encoding=utf-8
+#passwd=123456
+#print("陈土锋密码是：%d"%passwd)
+#print("请输入密码：")
+#a=input("请输入密码：")
+#print(a)
+#50.116.33.29  
+print("========================")
+Name=input("请输入你的姓名：")
+Qq=input("请输入你的QQ号：")
+Phone_Number=input("请输入你的手机号：")
+Address=input("公司地址：")
+print("正在为您打印**************")
+print("========================")
+sleep 10
+print("姓名：%d"%Name)
+
+print()
